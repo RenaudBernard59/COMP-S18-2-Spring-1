f06e4d7763cb4fe18825a15fa758661652bf2305
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-26 16:08:37 +0200

    DAO
    Create DAO

---
+++ src/main/java/tutoSpring/IListeCoursesDAO.java
@@ -0,0 +1,7 @@
+package tutoSpring;
+
+import java.util.List;
+
+public interface IListeCoursesDAO {
+	List<Course> rechercherCourses();
+}
---
+++ src/main/java/tutoSpring/IServiceListeCourses.java
@@ -0,0 +1,8 @@
+package tutoSpring;
+
+import java.util.List;
+
+public interface IServiceListeCourses {
+	List<Course> rechercherCourses();
+}
+
