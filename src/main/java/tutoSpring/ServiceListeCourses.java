bd250bc9cffaf812c3ed4d495fb0a95952c7fd33
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-30 11:18:35 +0200

    Debogue
    It's debogued.

--- src/main/java/tutoSpring/ServiceListeCourses.java
+++ src/main/java/tutoSpring/ServiceListeCourses.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ServiceListeCourses {
+public class ServiceListeCourses implements IServiceListeCourses {
 	@Autowired
 	private IListeCoursesDAO dao;
 	
